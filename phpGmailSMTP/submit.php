5b3eb51ca1f45f9672b07e632c3f68146f83dfc1
rejuwanulkarim <rejuwanulkarim@gmail.com>
2023-08-08 23:52:24 +0530

    php smtp

---
+++ phpGmailSMTP/submit.php
@@ -0,0 +1,41 @@
+<?php
+include('database.inc.php');
+$msg="";
+if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['mobile']) && isset($_POST['comment'])){
+	$name=mysqli_real_escape_string($con,$_POST['name']);
+	$email=mysqli_real_escape_string($con,$_POST['email']);
+	$mobile=mysqli_real_escape_string($con,$_POST['mobile']);
+	$comment=mysqli_real_escape_string($con,$_POST['comment']);
+	
+	mysqli_query($con,"insert into contact_us(name,email,mobile,comment) values('$name','$email','$mobile','$comment')");
+	$msg="Thanks message";
+	
+	$html="<p style='color:green;'><b>Your account created successfully</b></p>";
+	
+	include('smtp/PHPMailerAutoload.php');
+	$mail=new PHPMailer(true);
+	$mail->isSMTP();
+	$mail->Host="smtp.gmail.com";
+	$mail->Port=587;
+	$mail->SMTPSecure="tls";
+	$mail->SMTPAuth=true;
+	$mail->Username="karimdr341@gmail.com";
+	$mail->Password="brwbmksewtbbnkll";
+	$mail->SetFrom("karimdr341@gmail.com");
+	$mail->addAddress($email);
+	$mail->IsHTML(true);
+	$mail->Subject="New Contact Us";
+	$mail->Body=$html;
+	$mail->SMTPOptions=array('ssl'=>array(
+		'verify_peer'=>false,
+		'verify_peer_name'=>false,
+		'allow_self_signed'=>false
+	));
+	if($mail->send()){
+		//echo "Mail send";
+	}else{
+		//echo "Error occur";
+	}
+	echo $msg;
+}
+?>
