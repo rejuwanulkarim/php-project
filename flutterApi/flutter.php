04d745a284d794639649dc4692d1f8d3407286ca
rejuwanulkarim <rejuwanulkarim@gmail.com>
2023-08-08 23:49:30 +0530

    Flutter Api

---
+++ flutterApi/flutter.php
@@ -0,0 +1,41 @@
+<?php
+
+echo json_encode(array("product" => array(array(
+    "id" => 2,
+    "name" => "Chicken Roll",
+    "quantity" => "One Pice",
+    "price" => 13000,
+    "color" => "Black",
+    "image" => "https=>//th.bing.com/th/id/OIP.VaZP9cY9Vgd9juKp6IAflwHaFw?pid=ImgDet&rs=1"
+), array(
+    "id" => 2,
+    "name" => "Chicken Roll",
+    "quantity" => "One Pice",
+    "price" => 13000,
+    "color" => "Black",
+    "image" => "https=>//th.bing.com/th/id/OIP.VaZP9cY9Vgd9juKp6IAflwHaFw?pid=ImgDet&rs=1"
+), array(
+    "id" => 2,
+    "name" => "Chicken Roll",
+    "quantity" => "One Pice",
+    "price" => 13000,
+    "color" => "Black",
+    "image" => "https=>//th.bing.com/th/id/OIP.VaZP9cY9Vgd9juKp6IAflwHaFw?pid=ImgDet&rs=1"
+), array(
+    "id" => 2,
+    "name" => "Chicken Roll",
+    "quantity" => "One Pice",
+    "price" => 13000,
+    "color" => "Black",
+    "image" => "https=>//th.bing.com/th/id/OIP.VaZP9cY9Vgd9juKp6IAflwHaFw?pid=ImgDet&rs=1"
+), array(
+    "id" => 2,
+    "name" => "Chicken Roll",
+    "quantity" => "One Pice",
+    "price" => 13000,
+    "color" => "Black",
+    "image" => "https=>//th.bing.com/th/id/OIP.VaZP9cY9Vgd9juKp6IAflwHaFw?pid=ImgDet&rs=1"
+)
+
+
+)));
